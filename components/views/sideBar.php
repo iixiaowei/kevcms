bae124817519032b7d1d401adaf70e14d9640a5c
iixiaowei <iixiaowei@163.com>
2016-10-28 11:33:50 +0800

    Signed-off-by: iixiaowei <iixiaowei@163.com>

--- components/views/sideBar.php
+++ components/views/sideBar.php
@@ -1,476 +1,89 @@
 <ul class="nav nav-list">
-    <li class="">
-        <a href="index.html">
-            <i class="menu-icon fa fa-tachometer"></i>
-            <span class="menu-text"> Dashboard</span>
-        </a>
-
-        <b class="arrow"></b>
-    </li>
-
-    <li class="active open">
-        <a href="#" class="dropdown-toggle">
-            <i class="menu-icon fa fa-desktop"></i>
-							<span class="menu-text">
-								UI &amp; Elements
-							</span>
-
-            <b class="arrow fa fa-angle-down"></b>
-        </a>
-
-        <b class="arrow"></b>
-
-        <ul class="submenu">
-            <li class="active open">
+    <?php
+    $sql = "select * from {{menu}} where parentid=0 order by listorder desc";
+    $cmd=Yii::app()->db->createCommand($sql);
+    $results  = $cmd->queryAll();
+    foreach ($results as $rs){
+        $id=$rs['id'];
+        $sql = "select count(id) as cnt from {{menu}} where parentid=:parentid order by listorder desc";
+        $cmd=Yii::app()->db->createCommand($sql);
+        $cmd->bindParam(":parentid", $id,PDO::PARAM_INT);
+        $cnt = $cmd->queryScalar();
+        if (!in_array($id, $data)){
+            continue;
+        }
+        if ($cnt<=0){
+            ?>
+            <li <?php if( $this->menu==$rs['focus']){ echo 'class="active"'; } ?>>
+                <a href="<?php echo Yii::app()->baseUrl; ?>/admin">
+                    <i class="<?php echo $rs['icon']; ?>"></i>
+                    <span class="menu-text"> <?php echo $rs['name'] ?> </span>
+                </a>
+                <b class="arrow"></b>
+            </li>
+        <?php
+        }else{
+            ?>
+            <li <?php if( $this->menu==$rs['focus']){ echo 'class="active open"'; } ?>>
                 <a href="#" class="dropdown-toggle">
-                    <i class="menu-icon fa fa-caret-right"></i>
-
-                    Layouts
+                    <i class="<?php echo $rs['icon']; ?>"></i>
+                    <span class="menu-item-parent"><?php echo $rs['name'] ?></span>
                     <b class="arrow fa fa-angle-down"></b>
                 </a>
-
                 <b class="arrow"></b>
-
                 <ul class="submenu">
-                    <li class="">
-                        <a href="top-menu.html">
-                            <i class="menu-icon fa fa-caret-right"></i>
-                            Top Menu
-                        </a>
-
-                        <b class="arrow"></b>
-                    </li>
-
-                    <li class="active">
-                        <a href="two-menu-1.html">
-                            <i class="menu-icon fa fa-caret-right"></i>
-                            Two Menus 1
-                        </a>
-
-                        <b class="arrow"></b>
-                    </li>
-
-                    <li class="">
-                        <a href="two-menu-2.html">
-                            <i class="menu-icon fa fa-caret-right"></i>
-                            Two Menus 2
-                        </a>
-
-                        <b class="arrow"></b>
-                    </li>
-
-                    <li class="">
-                        <a href="mobile-menu-1.html">
-                            <i class="menu-icon fa fa-caret-right"></i>
-                            Default Mobile Menu
-                        </a>
-
-                        <b class="arrow"></b>
-                    </li>
-
-                    <li class="">
-                        <a href="mobile-menu-2.html">
-                            <i class="menu-icon fa fa-caret-right"></i>
-                            Mobile Menu 2
-                        </a>
-
-                        <b class="arrow"></b>
-                    </li>
-
-                    <li class="">
-                        <a href="mobile-menu-3.html">
-                            <i class="menu-icon fa fa-caret-right"></i>
-                            Mobile Menu 3
-                        </a>
-
-                        <b class="arrow"></b>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="">
-                <a href="typography.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Typography
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="elements.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Elements
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="buttons.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Buttons &amp; Icons
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="content-slider.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Content Sliders
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="treeview.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Treeview
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="jquery-ui.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    jQuery UI
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="nestable-list.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Nestable Lists
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="#" class="dropdown-toggle">
-                    <i class="menu-icon fa fa-caret-right"></i>
-
-                    Three Level Menu
-                    <b class="arrow fa fa-angle-down"></b>
-                </a>
-
-                <b class="arrow"></b>
-
-                <ul class="submenu">
-                    <li class="">
-                        <a href="#">
-                            <i class="menu-icon fa fa-leaf green"></i>
-                            Item #1
-                        </a>
-
-                        <b class="arrow"></b>
-                    </li>
-
-                    <li class="">
-                        <a href="#" class="dropdown-toggle">
-                            <i class="menu-icon fa fa-pencil orange"></i>
-
-                            4th level
-                            <b class="arrow fa fa-angle-down"></b>
-                        </a>
-
-                        <b class="arrow"></b>
-
+                    <?php
+                    $sql = "select * from {{menu}} where parentid=:parentid order by listorder desc";
+                    $cmd=Yii::app()->db->createCommand($sql);
+                    $cmd->bindParam(":parentid", $id,PDO::PARAM_INT);
+                    $res_sec = $cmd->queryAll();
+                    foreach ($res_sec as $r2){
+                    $id=$r2['id'];
+                    $sql = "select count(id) as cnt from {{menu}} where parentid=:parentid order by listorder desc";
+                    $cmd=Yii::app()->db->createCommand($sql);
+                    $cmd->bindParam(":parentid", $id,PDO::PARAM_INT);
+                    $cnt = $cmd->queryScalar();
+                    if (!in_array($id, $data)){
+                        continue;
+                    }
+                    if ($cnt<=0){
+                        ?>
+                        <li <?php if( $this->active==$r2['active']){ echo 'class="active"'; } ?>><a href="<?php echo Yii::app()->baseUrl; ?>/admin/<?php echo $r2['action']; ?>/<?php echo $r2['method']; ?>"><i
+                                    class="<?php echo $r2['icon']; ?>"></i><?php echo $r2['name']; ?></a></li>
+                    <?php
+                    }else{
+                    ?>
+                    <li>
+                        <a href="#"><i class="<?php echo $r2['icon']; ?>"></i>
+                            <span class="menu-item-parent"><?php echo $r2['name']; ?></span></a>
                         <ul class="submenu">
-                            <li class="">
-                                <a href="#">
-                                    <i class="menu-icon fa fa-plus purple"></i>
-                                    Add Product
-                                </a>
+                            <?php
+                            $sql = "select * from {{menu}} where parentid=:parentid order by listorder desc";
+                            $cmd=Yii::app()->db->createCommand($sql);
+                            $cmd->bindParam(":parentid", $id,PDO::PARAM_INT);
+                            $res_three = $cmd->queryAll();
+                            foreach ($res_three as $r3){
+                                $id = $r3['id'];
+                                if (!in_array($id, $data)){
+                                    continue;
+                                }
+                                ?>
+                                <li <?php if( $this->menu==$r3['focus']){ echo 'class="active"'; } ?>>
+                                    <a href="<?php echo Yii::app()->baseUrl; ?>/admin/<?php echo $r3['action']; ?>/<?php echo $r3['method']; ?>"><i class="<?php echo $r3['icon']; ?>"></i><?php echo $r3['name']; ?></a>
+                                </li>
 
-                                <b class="arrow"></b>
-                            </li>
-
-                            <li class="">
-                                <a href="#">
-                                    <i class="menu-icon fa fa-eye pink"></i>
-                                    View Products
-                                </a>
-
-                                <b class="arrow"></b>
-                            </li>
+                            <?php } ?>
                         </ul>
-                    </li>
+                        <?php
+                        }
+                        }
+                        ?>
                 </ul>
-            </li>
-        </ul>
-    </li>
 
-    <li class="">
-        <a href="#" class="dropdown-toggle">
-            <i class="menu-icon fa fa-list"></i>
-            <span class="menu-text"> Tables </span>
-
-            <b class="arrow fa fa-angle-down"></b>
-        </a>
-
-        <b class="arrow"></b>
-
-        <ul class="submenu">
-            <li class="">
-                <a href="tables.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Simple &amp; Dynamic
-                </a>
-
-                <b class="arrow"></b>
             </li>
 
-            <li class="">
-                <a href="jqgrid.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    jqGrid plugin
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-        </ul>
-    </li>
-
-    <li class="">
-        <a href="#" class="dropdown-toggle">
-            <i class="menu-icon fa fa-pencil-square-o"></i>
-            <span class="menu-text"> Forms </span>
-
-            <b class="arrow fa fa-angle-down"></b>
-        </a>
-
-        <b class="arrow"></b>
-
-        <ul class="submenu">
-            <li class="">
-                <a href="form-elements.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Form Elements
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="form-elements-2.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Form Elements 2
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="form-wizard.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Wizard &amp; Validation
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="wysiwyg.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Wysiwyg &amp; Markdown
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="dropzone.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Dropzone File Upload
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-        </ul>
-    </li>
-
-    <li class="">
-        <a href="widgets.html">
-            <i class="menu-icon fa fa-list-alt"></i>
-            <span class="menu-text"> Widgets </span>
-        </a>
-
-        <b class="arrow"></b>
-    </li>
-
-    <li class="">
-        <a href="calendar.html">
-            <i class="menu-icon fa fa-calendar"></i>
-
-							<span class="menu-text">
-								Calendar
-
-								<!-- #section:basics/sidebar.layout.badge -->
-								<span class="badge badge-transparent tooltip-error" title="2 Important Events">
-									<i class="ace-icon fa fa-exclamation-triangle red bigger-130"></i>
-								</span>
-
-								<!-- /section:basics/sidebar.layout.badge -->
-							</span>
-        </a>
-
-        <b class="arrow"></b>
-    </li>
-
-    <li class="">
-        <a href="gallery.html">
-            <i class="menu-icon fa fa-picture-o"></i>
-            <span class="menu-text"> Gallery </span>
-        </a>
-
-        <b class="arrow"></b>
-    </li>
-
-    <li class="">
-        <a href="#" class="dropdown-toggle">
-            <i class="menu-icon fa fa-tag"></i>
-            <span class="menu-text"> More Pages </span>
-
-            <b class="arrow fa fa-angle-down"></b>
-        </a>
-
-        <b class="arrow"></b>
-
-        <ul class="submenu">
-            <li class="">
-                <a href="profile.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    User Profile
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="inbox.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Inbox
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="pricing.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Pricing Tables
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="invoice.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Invoice
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="timeline.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Timeline
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="email.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Email Templates
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="login.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Login &amp; Register
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-        </ul>
-    </li>
-
-    <li class="">
-        <a href="#" class="dropdown-toggle">
-            <i class="menu-icon fa fa-file-o"></i>
-
-							<span class="menu-text">
-								Other Pages
-
-								<!-- #section:basics/sidebar.layout.badge -->
-								<span class="badge badge-primary">5</span>
-
-								<!-- /section:basics/sidebar.layout.badge -->
-							</span>
-
-            <b class="arrow fa fa-angle-down"></b>
-        </a>
-
-        <b class="arrow"></b>
-
-        <ul class="submenu">
-            <li class="">
-                <a href="faq.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    FAQ
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="error-404.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Error 404
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="error-500.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Error 500
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="grid.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Grid
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-
-            <li class="">
-                <a href="blank.html">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    Blank Page
-                </a>
-
-                <b class="arrow"></b>
-            </li>
-        </ul>
-    </li>
-</ul><!-- /.nav-list -->
+        <?php
+        }
+    }
+    ?>
+</ul>
